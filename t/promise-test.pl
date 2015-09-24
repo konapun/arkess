f95c81186b04a7f647fb0ace555044c32bdec887
konapun <bremen.braun@gmail.com>
2015-09-23 21:17:00 -0500

    Added promise test

--- t/promise-test.pl
+++ t/promise-test.pl
@@ -1,27 +1,81 @@
 #!/usr/bin/perl
 
 use strict;
-use lib '../lib';
+use lib '../lib/';
 use Arkess::Event::Promise;
 
-# my $promise = Arkess::Event::Promise->new(sub {
-#   my ($resolve, $reject) = @_;
-#
-#   if (1) {
-#     $resolve->('ACCEPT!');
-#   }
-#   else {
-#     $reject->('REJECT!');
-#   }
-# })->then(sub {
-#   print "Promise 2\n";
-# })->then(sub {
-#   print "Promise 3\n";
-# });
+sub doSomething {
+  return Arkess::Event::Promise->new(sub {
+    my ($resolve, $reject) = @_;
 
-my $promise = Arkess::Event::Promise->resolve(1);
+    my $value = 42;
+    if ($value == 0) {
+      $reject->($value);
+    }
+    else {
+      $resolve->($value);
+    }
+  });
+}
+
+sub doSomethingElse {
+  return Arkess::Event::Promise->new(sub {
+    my $resolve = shift;
+
+    my $value = 69;
+    $resolve->($value);
+  });
+}
+
+my $promise = doSomething();
 $promise->then(sub {
   my $value = shift;
 
-  print "SUCCEEDED with value $value!\n";
+  print "Got a value: $value\n";
+});
+$promise->then(sub {
+  my $value = shift;
+
+  print "Got the same value again: $value\n";
+});
+
+doSomething()->then(sub {
+  my $result = shift;
+
+  print "First result: $result\n";
+  return 88;
+})->then(sub {
+  my $result = shift;
+
+  print "Second result: $result\n";
+});
+
+doSomething()->then()->then(sub {
+  my $result = shift;
+
+  print "Got a result $result\n";
+});
+
+doSomething()->then(sub {
+  my $result = shift;
+
+  return doSomethingElse($result);
+})->then(sub {
+  my $finalResult = shift;
+
+  print "The final result is $finalResult\n";
+});
+
+print "\n################\n";
+my $resolved = Arkess::Event::Promise->resolve(99);
+$resolved->then(sub {
+  my $value = shift;
+
+  print "Resolved function with value $value\n";
+});
+my $rejected = Arkess::Event::Promise->reject("Testing reject");
+$rejected->catch(sub {
+  my $reason = shift;
+
+  print "Rejected because: $reason\n";
 });
